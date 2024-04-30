d14f2cd0755c0276f6bfaac73cdb1953ceb25ff2
JailtonJunior14 <jailtonjrbr@gmail.com>
2024-04-30 10:52:43 -0300

    Adicionar arquivos de projeto.

---
+++ ArrayMeses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMeses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = {"","Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+            Console.WriteLine("Digite o numero de um mês: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mês selecionado: " + meses[i]);
+            Console.ReadKey();
+        }
+    }
+}
